bfaf4d19ffd094522fbf67173476640127fd507e
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-10-12 00:23:13 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  "SELECT MAX(gpa) FROM students LIMIT 1"
+  "SELECT MAX(gpa) FROM students LIMIT 1;"
 end
 
 def lowest_student_gpa
